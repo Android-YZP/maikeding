cac35e12adde606184d85f4fc66d0379f8354e3c
zzzmfx <2885623312@qq.com>
2016-11-18 09:50:07 +0800

    首页界面布局调整

--- uikit/src/com/netease/nim/uikit/common/activity/UI.java
+++ uikit/src/com/netease/nim/uikit/common/activity/UI.java
@@ -14,6 +14,7 @@ import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import com.netease.nim.uikit.common.fragment.TFragment;
 import com.netease.nim.uikit.common.util.log.LogUtil;
@@ -30,6 +31,7 @@ public abstract class UI extends AppCompatActivity {
     private static Handler handler;
 
     private Toolbar toolbar;
+    private TextView textView;
 
     @Override
     protected void onStart() {
@@ -100,10 +102,17 @@ public abstract class UI extends AppCompatActivity {
         }
     }
 
-    public void setToolBar(int toolbarId, int titleId, int logoId) {
+    public void setToolBar(int toolbarId, int toolTitle, int titleId) {
+        toolbar = (Toolbar) findViewById(toolbarId);
+        toolbar.setTitle("");
+        textView = (TextView) findViewById(toolTitle);
+        textView.setText(titleId);
+        setSupportActionBar(toolbar);
+    }
+
+    public void setToolBar(int toolbarId, int titleId) {
         toolbar = (Toolbar) findViewById(toolbarId);
         toolbar.setTitle(titleId);
-        toolbar.setLogo(logoId);
         setSupportActionBar(toolbar);
     }
 
--- uikit/src/com/netease/nim/uikit/common/fragment/TFragment.java
+++ uikit/src/com/netease/nim/uikit/common/fragment/TFragment.java
@@ -128,11 +128,18 @@ public abstract class TFragment extends Fragment {
         return (T) (getView().findViewById(resId));
     }
 
-    protected void setToolBar(int toolbarId, int titleId, int logoId) {
+    protected void setToolBar(int toolbarId, int titleId) {
         if (getActivity() != null && getActivity() instanceof UI) {
-            ((UI)getActivity()).setToolBar(toolbarId, titleId, logoId);
+            ((UI)getActivity()).setToolBar(toolbarId, titleId);
         }
     }
+
+    protected void setToolBar(int toolbarId, int toolTitle, int titleId) {
+        if (getActivity() != null && getActivity() instanceof UI) {
+            ((UI)getActivity()).setToolBar(toolbarId, toolTitle,titleId);
+        }
+    }
+
     protected void setTitle(int titleId) {
         if (getActivity() != null && getActivity() instanceof UI) {
             getActivity().setTitle(titleId);
