d9dbf97fb8eb93ce3df1c04262e923592704e185
zzzmfx <2885623312@qq.com>
2016-11-28 14:26:00 +0800

    test

--- demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
+++ demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -11,6 +11,6 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 29;
   public static final String VERSION_NAME = "3.1.0";
   // Fields from default config.
-  public static final String BUILD_DATE = "2016-11-28 8:35:53";
+  public static final String BUILD_DATE = "2016-11-28 9:12:36";
   public static final String GIT_REVISION = "";
 }
