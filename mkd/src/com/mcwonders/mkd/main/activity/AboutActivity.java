4cf947c8e2668afb09473d7d4a7f66a0fcecfe15
androidYZP <18626312413@163.com>
2016-12-16 14:08:43 +0800

    添加闪屏页

--- mkd/src/com/mcwonders/mkd/main/activity/AboutActivity.java
+++ mkd/src/com/mcwonders/mkd/main/activity/AboutActivity.java
@@ -11,18 +11,18 @@ import com.mcwonders.uikit.model.ToolBarOptions;
 
 public class AboutActivity extends UI {
 
-	private TextView versionGit;
-	private TextView versionDate;
+    private TextView versionGit;
+    private TextView versionDate;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(com.mcwonders.mkd.R.layout.about_layout);
-		ToolBarOptions options = new ToolBarOptions();
-		setToolBar(R.id.toolbar, options, R.id.toolbar_about_title);
-		findViews();
-		initViewData();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.mcwonders.mkd.R.layout.about_layout);
+        ToolBarOptions options = new ToolBarOptions();
+        setToolBar(R.id.toolbar, options, R.id.toolbar_about_title);
+        findViews();
+        initViewData();
+    }
 
     @Override
     protected void onPause() {
@@ -30,23 +30,23 @@ public class AboutActivity extends UI {
     }
 
     private void findViews() {
-		versionGit = (TextView) findViewById(com.mcwonders.mkd.R.id.version_detail_git);
-		versionDate = (TextView) findViewById(com.mcwonders.mkd.R.id.version_detail_date);
+        versionGit = (TextView) findViewById(com.mcwonders.mkd.R.id.version_detail_git);
+        versionDate = (TextView) findViewById(com.mcwonders.mkd.R.id.version_detail_date);
 
 //        CustomActions.customButton((Button) findViewById(R.id.about_custom_button_1));
-	}
+    }
 
-	private void initViewData() {
+    private void initViewData() {
         // 如果使用的IDE是Eclipse， 将该函数体注释掉。这里使用了Android Studio编译期添加BuildConfig字段的特性
-		try {
-			String pkName = this.getPackageName();
-			String versionName = this.getPackageManager()
+        try {
+            String pkName = this.getPackageName();
+            String versionName = this.getPackageManager()
                     .getPackageInfo(pkName, 0).versionName;
-			versionGit.setText(" Version: V "+versionName );
-		} catch (PackageManager.NameNotFoundException e) {
-			e.printStackTrace();
-		}
+            versionGit.setText(" Version: V " + versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
 //		versionDate.setText("Build Date:" + BuildConfig.BUILD_DATE);
-	}
+    }
 }
